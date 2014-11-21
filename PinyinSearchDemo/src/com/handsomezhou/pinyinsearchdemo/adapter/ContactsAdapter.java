31ffe78034534c6408214fad4f5c8af2a13778ff
handsomezhou <quanjunzhou@gmail.com>
2014-11-21 13:09:41 +0800

    update

--- PinyinSearchDemo/src/com/handsomezhou/pinyinsearchdemo/adapter/ContactsAdapter.java
+++ PinyinSearchDemo/src/com/handsomezhou/pinyinsearchdemo/adapter/ContactsAdapter.java
@@ -3,8 +3,6 @@ package com.handsomezhou.pinyinsearchdemo.adapter;
 import java.util.List;
 
 import android.content.Context;
-import android.text.Html;
-import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
