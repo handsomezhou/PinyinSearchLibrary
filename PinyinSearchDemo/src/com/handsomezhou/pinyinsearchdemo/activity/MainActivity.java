22f69d1d3ee812017679d8bbc3caf3938f0b70c6
handsomezhou <quanjunzhou@gmail.com>
2015-05-23 22:30:29 +0800

    add fragment

--- PinyinSearchDemo/src/com/handsomezhou/pinyinsearchdemo/activity/MainActivity.java
+++ PinyinSearchDemo/src/com/handsomezhou/pinyinsearchdemo/activity/MainActivity.java
@@ -1,71 +1,20 @@
 package com.handsomezhou.pinyinsearchdemo.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.app.Fragment;
 
-import com.handsomezhou.pinyinsearchdemo.R;
+import com.handsomezhou.pinyinsearchdemo.fragment.MainFragment;
 
 
-/**
- * @description Main activity
- * @author handsomezhou
- * @date 2014.11.09
- */
+public class MainActivity extends BaseSingleFragmentActivity{
 
-public class MainActivity extends Activity{
-	//private static final String TAG = "MainActivity";
-	private Context mContext;
-	private Button mT9SearchBtn;
-	private Button mQwertySearchBtn;
-	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mContext = this;
-		initView();
-		initData();
-		initListener();
-	}
-	
-	private void initView(){
-		mT9SearchBtn=(Button) findViewById(R.id.t9_search_btn);
-		mQwertySearchBtn=(Button) findViewById(R.id.qwerty_search_btn);
-	}
-	
-	private void initData(){
-		
-	}
-	
-	private void initListener(){
-		mT9SearchBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				startT9Search();
-			}
-		});
-		
-		mQwertySearchBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				startQwertySearch();
-			}
-		});
-	}
-	
-	private void startT9Search(){
-		Intent intent=new Intent(mContext, T9SearchActivity.class);
-		startActivity(intent);
+	protected Fragment createFragment() {
+		return new MainFragment();
 	}
 
-	private void startQwertySearch(){
-		Intent intent=new Intent(mContext, QwertySearchActivity.class);
-		startActivity(intent);
+	@Override
+	protected boolean isRealTimeLoadFragment() {
+		return false;
 	}
+	
 }
--- PinyinSearchDemo/src/com/handsomezhou/pinyinsearchdemo/activity/QwertySearchActivity.java
+++ PinyinSearchDemo/src/com/handsomezhou/pinyinsearchdemo/activity/QwertySearchActivity.java
@@ -1,176 +1,21 @@
 package com.handsomezhou.pinyinsearchdemo.activity;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.support.v4.app.Fragment;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.Toast;
+import com.handsomezhou.pinyinsearchdemo.fragment.QwertySearchFragment;
 
-import com.handsomezhou.pinyinsearchdemo.R;
-import com.handsomezhou.pinyinsearchdemo.helper.ContactsHelper;
-import com.handsomezhou.pinyinsearchdemo.helper.ContactsIndexHelper;
-import com.handsomezhou.pinyinsearchdemo.helper.ContactsHelper.OnContactsLoad;
-import com.handsomezhou.pinyinsearchdemo.model.Contacts;
-import com.handsomezhou.pinyinsearchdemo.util.ShareUtil;
-import com.handsomezhou.pinyinsearchdemo.view.ContactsOperationView;
-import com.handsomezhou.pinyinsearchdemo.view.ContactsOperationView.OnContactsOperationView;
+public class QwertySearchActivity extends BaseSingleFragmentActivity{
 
-public class QwertySearchActivity extends Activity implements OnContactsLoad,OnContactsOperationView{
-	private static final String TAG="QwertySearchActivity";
-	private Context mContext;
-	private ContactsOperationView mContactsOperationView;
-	private EditText mSearchEt;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_qwerty_search);
-		mContext=this;
-		initView();
-		initData();
-		initListener();
+	protected Fragment createFragment() {
+
+		return new QwertySearchFragment();
+	}
+
+	@Override
+	protected boolean isRealTimeLoadFragment() {
+
+		return false;
 	}
 	
-	
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-		
-		mContactsOperationView.updateContactsList();
-	}
-
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		
-		mSearchEt.setText("");
-		ContactsHelper.getInstance().parseQwertyInputSearchContacts(null);
-		
-		List<Contacts> selectedContactsList=new ArrayList<Contacts>();
-		selectedContactsList.addAll(ContactsHelper.getInstance().getSelectedContacts().values());
-		Log.i(TAG, "onDestroy() selectedContactsList.size()="+selectedContactsList.size());
-		for(Contacts cs:selectedContactsList){
-			Log.i(TAG, "onDestroy() name=["+cs.getName()+"] phoneNumber=["+cs.getPhoneNumber()+"]");
-		}
-		
-		mContactsOperationView.clearSelectedContacts();
-		ContactsHelper.getInstance().clearSelectedContacts();
-	}
-
-
-	private void initView(){
-		mSearchEt=(EditText)findViewById(R.id.search_edit_text);
-		mContactsOperationView = (ContactsOperationView)findViewById(R.id.contacts_operation_layout);
-		mContactsOperationView.setOnContactsOperationView(this);
-	}
-	
-	private void initData(){
-		ContactsHelper.getInstance().setOnContactsLoad(this);
-		boolean startLoad = ContactsHelper.getInstance().startLoadContacts();
-		if (true == startLoad) {
-			mContactsOperationView.contactsLoading();
-		}
-	}
-	
-	private void initListener(){
-		mSearchEt.addTextChangedListener(new TextWatcher() {
-			
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void afterTextChanged(Editable s) {
-				String curCharacter=s.toString().trim();
-				
-				if(TextUtils.isEmpty(curCharacter)){
-					ContactsHelper.getInstance().parseQwertyInputSearchContacts(null);
-				}else{
-					ContactsHelper.getInstance().parseQwertyInputSearchContacts(curCharacter);
-				}
-				mContactsOperationView.updateContactsList(TextUtils.isEmpty(curCharacter));
-				
-			}
-		});
-	}
-	
-	/*start:OnContactsLoad*/
-	@Override
-	public void onContactsLoadSuccess() {
-		ContactsHelper.getInstance().parseQwertyInputSearchContacts(null);
-		mContactsOperationView.contactsLoadSuccess();
-		
-		//just background printing contacts information
-		//ContactsHelper.getInstance().showContactsInfo();
-		ContactsIndexHelper.getInstance().praseContacts(ContactsHelper.getInstance().getBaseContacts());
-		//ContactsIndexHelper.getInstance().showContactsInfo();
-	}
-
-	@Override
-	public void onContactsLoadFailed() {
-		mContactsOperationView.contactsLoadFailed();
-	}
-	/*end:OnContactsLoad*/
-
-	/*start:OnContactsOperationView*/
-	@Override
-	public void onListItemClick(Contacts contacts,int position){
-		ContactsHelper.getInstance().parseQwertyInputSearchContacts(null);
-		mContactsOperationView.updateContactsList(true);
-	}
-
-	@Override
-	public void onAddContactsSelected(Contacts contacts) {
-		if(null!=contacts){
-			Log.i(TAG, "onAddContactsSelected name=["+contacts.getName()+"] phoneNumber=["+contacts.getPhoneNumber()+"]");
-			Toast.makeText(mContext,"Add ["+contacts.getName()+":"+contacts.getPhoneNumber()+"]", Toast.LENGTH_SHORT).show();
-			ContactsHelper.getInstance().addSelectedContacts(contacts);
-		}
-	}
-
-
-	@Override
-	public void onRemoveContactsSelected(Contacts contacts) {
-		if(null!=contacts){
-			Log.i(TAG, "onRemoveContactsSelected name=["+contacts.getName()+"] phoneNumber=["+contacts.getPhoneNumber()+"]");
-			Toast.makeText(mContext,"Remove ["+contacts.getName()+":"+contacts.getPhoneNumber()+"]", Toast.LENGTH_SHORT).show();
-			ContactsHelper.getInstance().removeSelectedContacts(contacts);
-		}
-	}
-	
-	@Override
-	public void onContactsCall(Contacts contacts) {
-		//Toast.makeText(mContext, "onContactsCall"+contacts.getPhoneNumber(), Toast.LENGTH_SHORT).show();
-		if(null!=contacts){
-			 Intent intent = new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+contacts.getPhoneNumber()));
-			 mContext.startActivity(intent);
-		}
-	}
-
-
-	@Override
-	public void onContactsSms(Contacts contacts) {
-		//Toast.makeText(mContext, "onContactsSms"+contacts.getPhoneNumber(), Toast.LENGTH_SHORT).show();
-		ShareUtil.shareTextBySms(mContext, contacts.getPhoneNumber(), null);
-	}
-	/*end:OnContactsOperationView*/
-
 }
